0d8cc22ae01c21946c08667717dba047893d0198
Jaafar Bendahou <jaafar.bendahou@gmail.com>
2024-04-04 17:20:57 +0000

    début hero et gsap

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,22 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
 
-export default Hero
+  useGSAP(() => {
+    gsap.to("#hero", {
+      opacity: 1,
+      delay: 1.5
+    });
+  }, []); //[] = quand l'animation va se déclencher: si vide = dès qu'on actualise la page
+
+  return (
+    <section className="w-full nav-height bg-black relative">
+      <div className="h-5/6 w-full flex-center flex-col">
+        <p id='hero' className="hero-title">iPhone 15 Pro</p>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
