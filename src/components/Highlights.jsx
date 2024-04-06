8b50f0682b00a54f63e32df3355ebff34d2bf8b2
Jaafar Bendahou <jaafar.bendahou@gmail.com>
2024-04-06 01:37:05 +0000

    fin highlights part

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,20 +1,21 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-import { watchImg } from "../utils";
+import { rightImg, watchImg } from "../utils";
 
 const Highlights = () => {
-
-  useGSAP(()=>{
-    gsap.to('#title', {
+  useGSAP(() => {
+    gsap.to("#title", {
       opacity: 1,
-      y:0
-    })
+      y: 0,
+    });
 
-    gsap.to('.link', {
-      opacity:1,
-      y:0
-    })
-  }, [])
+    gsap.to(".link", {
+      opacity: 1,
+      y: 0,
+      duration:1,
+      stagger:0.25 //Ecart de début d'animation entre les 2 élements avec className="link"
+    });
+  }, []);
 
   return (
     <section
@@ -22,13 +23,19 @@ const Highlights = () => {
       className="w-screen overflow-hidden h-full common-padding bg-zinc"
     >
       <div className="screen-max-width">
-        <div className="mb-12 w-full items-end justify-between">
-          <h1 id="title" className="section-heading">Get the highlights.</h1>
+        <div className="md:flex mb-12 w-full items-end justify-between">
+          <h1 id="title" className="section-heading">
+            Get the highlights.
+          </h1>
           <div className="flex flex-wrap items-end gap-5">
             <p className="link">
               Watch the film
               <img src={watchImg} alt="watch" className="ml-2" />
-              </p>
+            </p>
+            <p className="link">
+              Watch the event
+              <img src={rightImg} alt="right" className="ml-2" />
+            </p>
           </div>
         </div>
       </div>
