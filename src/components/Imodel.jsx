cbcc04f2a5a4d40a049ce66172fd62e2c6631578
Jaafar Bendahou <jaafar.bendahou@gmail.com>
2024-04-18 19:19:30 +0100

    fin landing page Apple

--- src/components/Imodel.jsx
+++ src/components/Imodel.jsx
@@ -4,7 +4,7 @@ import ScrollTrigger from "gsap/ScrollTrigger";
 import { useState } from "react";
 import iphoneNatural from "/assets/images/iphone-natural-none.png"
 
-import { models, sizes } from "../constants";
+import { models } from "../constants";
 
 
 gsap.registerPlugin(ScrollTrigger);
@@ -21,8 +21,8 @@ const Imodel = () => {
     });
   }, []);
 
-  //To choose if we want to look at the small or big iphone 3D Model
-  const [size, setSize] = useState("small");
+  // To choose if we want to look at the small or big iphone 3D Model
+  // const [size, setSize] = useState("small");
 
   //All the details properties for the 3D models, small of big iPhone
   const [model, setModel] = useState({
@@ -40,27 +40,27 @@ const Imodel = () => {
 
         {/* h-[75vh]= 3/4 de l'écran /overflow-hidden: pour qu'il n'y ait pas de scrollbar*/}
         <div className="flex flex-col items-center mt-5">
-          <div className="w-full h-[75vh] md:h-[90vh] border border-white  ">
-           <img src={model.img} alt="image" className="border border-red-500" />
+          <div className="flex justify-center items-center w-full max-sm:h-[45vh] sm:h-[60vh]">
+           <img src={model.img} alt="image" />
           </div>
           <div className="mx-auto w-full">
             <p className="text-sm font-light text-center mb-5">{model.title}</p>
             <div className="flex-center">
               {/* To map over all the colors that we can choose for the models / ul va etre le container gris des petits points avec les couleurs qui vont etre les <li>*/}
               <ul className="color-container">
-                {models.map((e, i)=>( 
-                  <li key={i} className="w-6 h-6 rounded-full mx-2 cursor-pointer" style={{backgroundColor: e.color[0]}} onClick={()=>setModel(e)}> 
+                {models.map((e, i)=>(
+                  <li key={i} className="w-6 h-6 rounded-full mx-2 cursor-pointer" style={{backgroundColor: e.color[0]}} onClick={()=>setModel(e)}>
                   </li>
                 ))}
               </ul>
 
-              <button className="size-btn-container">
+              {/* <button className="size-btn-container">
                 {sizes.map(({label, value}) => ( //!!!!! on destructure les properties label et values pour pouvoir les utiliser dans .map / OnClick est ce qui permet de changer la value de la state: size et donc de changer les properties (backgroundColor et color) des boutons séléctionnés. state aura même valeu que bouton selectioné
                   <span key={label} className="size-btn" style={{backgroundColor: size===value ?'white':'transparent', color: size===value ? 'black' : 'white'}} onClick={()=>setSize(value)} >
                     {label}
                   </span>
                 ) )}
-              </button>
+              </button> */}
             </div>
           </div>
         </div>
